8e9b9296723bb137af05a8f042423248b89dd71c
gokcedemiral <egokced@gmail.com>
2022-02-08 11:38:14 +0100

    Nuxt project scaffolding

---
+++ first-project-with-nuxt/pages/Product.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1>Product Page!</h1>
+    <p>
+      <nuxt-link to="/">
+        Home
+      </nuxt-link>
+    </p>
+  </div>
+</template>
+
+<style>
+  .container {
+    font-family: "Quicksand", "Source Sans Pro";
+    padding: 60px;
+  }
+</style>
---
+++ first-project-with-nuxt/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Welcome!</h1>
+    <p><nuxt-link to="/product">Product page</nuxt-link></p>
+  </div>
+</template>
+
+<style>
+  .container {
+    font-family: "Quicksand", "Source Sans Pro";
+    padding: 60px;
+  }
+</style>
